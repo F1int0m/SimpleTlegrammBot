43194b7d4c3a893c5c5eea97c21a6a42187dc89e
Nikita <buguev.nikita@gmail.com>
2020-03-11 10:21:40 +0500

    123

--- TlegramBot/Models/AppSettings.cs
+++ TlegramBot/Models/AppSettings.cs
@@ -4,8 +4,8 @@ namespace TelegramBot.Models
     public static class AppSettings
     {
 
-        public static string Url { get; set; } = "https://telegrambotrtf.azurewebsites.net:443/{0}";
-        public static string Name { get; set; } = "superSecretRtF_bot";
-        public static string Key { get; set; } = "1046637017:AAEpfs_z9Alec9yKF4LnaZBql4lB9kaWXmI";
+        public static string Url { get; set; } = "Host URL";
+        public static string Name { get; set; } = "Name";
+        public static string Key { get; set; } = "Token";
     }
 }
